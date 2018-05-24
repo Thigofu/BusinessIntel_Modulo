bbd63993c6446ec87dc958c13daa46c70cfeacae
Thiago Leon <thigofu@gmail.com>
2018-05-24 19:38:33 -0300

    Adicionado graficos e export para PDF

--- dtm.py
+++ dtm.py
@@ -1,3 +1,15 @@
+#Importando a biblioteca para criação de graficos visuais
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
+
+#função que faz a conversão para porcentagem e exibe texto nos graficos
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return '{p:.2f}%  ({v:d})'.format(p=pct,v=val)
+    return my_autopct
 #Importando o arquivo .CSV para analise em modo de leitura #
 clientes = open('clientes.csv', 'r')
 funcionarios = open('func.csv', 'r')
@@ -117,6 +129,7 @@ porcentosim = (contadorsim / Tamanholista) * 100
 porcentonao = (contadornao / Tamanholista) * 100
 print("Total de atendimentos presenciais solucionados =", contadorsim,"Que equivalem a",round(porcentosim),"%")
 print("Total de atendimentos presenciais não solucionados =", contadornao,"Que equivalem a",round(porcentonao),"%\n")
+
 print("Dos",contadornao,"casos não solucionados:")
 print(boleto,"Eram Boletos")
 print(carimbo,"Eram Carimbos/Passe escolar")
@@ -124,7 +137,12 @@ print(requerimento,"Eram Requerimentos")
 print(problema,"Eram Problemas com matricula")
 print(documentos,"Eram Entrega/Retirada documentos")
 print(trancamento,"Eram trancamento/Cancelamento de matricula\n\n")
-
+grafnotsolbol = boleto
+grafnotsolcarimbo = carimbo
+grafnotsolrequ = requerimento
+grafnotsolprob = problema
+grafnotsoldoc = documentos
+grafnotsoltranc = trancamento
 #=========================================== Contador de solucao presencial =============================
 
 #========================================== Contador de solucao telefonica =============================
@@ -172,6 +190,12 @@ print(requerimento,"Eram Requerimentos")
 print(problema,"Eram Problemas com matricula")
 print(documentos,"Eram Entrega/Retirada documentos")
 print(trancamento,"Eram trancamento/Cancelamento de matricula\n\n")
+grafnotsolcelbol = boleto
+grafnotsolcelcarimbo = carimbo
+grafnotsolcelrequ = requerimento
+grafnotsolcelprob = problema
+grafnotsolceldoc = documentos
+grafnotsolceltranc = trancamento
 
 #=========================================== Contador de solucao telefonica =============================
 
@@ -271,10 +295,6 @@ for i in range(1, (Tamanholistafunc), 1):
             sem3 += 1
         if (semana[k] == "Quarta semana"):
             sem4 += 1
-
-
-
-
     
 print("Pessoas que frequentam no horario das 15:00 -->",hora1," representam:",((hora1 / Tamanholista)*100),"%")
 print("Pessoas que frequentam no horario das 16:00 -->",hora2," representam:",((hora2 / Tamanholista)*100),"%")
@@ -351,11 +371,16 @@ for i in range(1, (Tamanholista), 1):
     else:
         resp5 +=1
 
-print("atendimentos que demoram menos de 10 minutos =",resp1," representam:",((resp1 / Tamanholista)*100),"%")
-print("atendimentos que demoram de 10 a 30 minutos =",resp2," representam:",((resp2 / Tamanholista)*100),"%")
-print("atendimentos que demoram de 30 a 45 minutos =",resp3," representam:",((resp3 / Tamanholista)*100),"%")
-print("atendimentos que demoram 1 hora =",resp4," representam:",((resp4 / Tamanholista)*100),"%")
-print("atendimentos que demoram mais de 1 hora =",resp5," representam:",((resp5 / Tamanholista)*100),"%\n")
+pcresp1 = ((resp1 / Tamanholista)*100)
+pcresp2 = ((resp2 / Tamanholista)*100)
+pcresp3 = ((resp3 / Tamanholista)*100)
+pcresp4 = ((resp4 / Tamanholista)*100)
+pcresp5 = ((resp5 / Tamanholista)*100)
+print("atendimentos que demoram menos de 10 minutos =",resp1," representam:",pcresp1,"%")
+print("atendimentos que demoram de 10 a 30 minutos =",resp2," representam:",pcresp2,"%")
+print("atendimentos que demoram de 30 a 45 minutos =",resp3," representam:",pcresp3,"%")
+print("atendimentos que demoram 1 hora =",resp4," representam:",pcresp4,"%")
+print("atendimentos que demoram mais de 1 hora =",resp5," representam:",pcresp5,"%\n")
 
 #======================================== Contador de tempo de demora atendimento presencial ====================================
 
@@ -382,6 +407,13 @@ for i in range(1, (Tamanholista), 1):
         resp4 += 1
     else:
         resp5 +=1
+pcrespcel1 = ((resp1 / Tamanholista)*100)
+pcrespcel2 = ((resp2 / Tamanholista)*100)
+pcrespcel3 = ((resp3 / Tamanholista)*100)
+pcrespcel4 = ((resp4 / Tamanholista)*100)
+pcrespcel5 = ((resp5 / Tamanholista)*100)
+
+
 
 print("atendimentos telefonicos que demoram menos de 10 minutos =",resp1," representam:",((resp1 / Tamanholista)*100),"%")
 print("atendimentos telefonicos que demoram de 10 a 30 minutos =",resp2," representam:",((resp2 / Tamanholista)*100),"%")
@@ -425,7 +457,12 @@ print("Requerimentos =",requerimento)
 print("Problemas com matricula =",problema)
 print("Entrega/Retirada Documentos =",documentos)
 print("Cancelamento/Trancamento Matricula =",trancamento)
-
+grafservbol = boleto
+grafservcar = carimbo
+grafservreq = requerimento
+grafservprob = problema
+grafservdoc = documentos
+grafservtranc = trancamento
 #======================================== Contador de satisfacao dos funcionarios ======================================================
 print("#======================================== Contador de satisfacao dos funcionarios ======================================================")
 
@@ -489,3 +526,175 @@ print("Equipe:",equipe)
 print("Equipamentos:", equipamentos)
 print("Procedimentos:", procedimentos)
 print("Coordenação:",coordenacao)
+
+
+#=============================================================================================================================================
+#                                                            SETOR DOS GRAFICOS                                                                         
+#=============================================================================================================================================
+
+#=====================================Grafico de Pizza das soluções=======================================================
+nomes = 'Sim', 'Não'
+tamanho = [porcentosim , porcentonao ]
+fig1, grafsol = plt.subplots()
+grafsol.set_title("Contagem de solução presencial")
+grafsol.pie(tamanho, autopct='%1.1f%%',shadow=False, startangle=90)
+grafsol.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+grafsol.legend(nomes)
+#=====================================Grafico de Pizza das soluções=======================================================
+
+
+#=====================================Grafico de Pizza das soluções Telefonicas=======================================================
+nomes = 'Sim', 'Não'
+tamanho = [porcentocelsim , porcentocelnao ]
+fig2, grafsolcel = plt.subplots()
+grafsolcel.set_title("Contagem de solução Telefonica")
+grafsolcel.pie(tamanho, autopct='%1.1f%%',shadow=False, startangle=90)
+grafsolcel.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+grafsolcel.legend(nomes)
+#=====================================Grafico de Pizza das soluções Telefonicas=======================================================
+
+#=====================================Grafico de Pizza do tempo demora solucoes=======================================================
+nomes = '< 10 minutos', '10 - 30 minutos', '30 - 45 minutos', '1 hora', '> 1 hora'
+tamanho = [pcresp1, pcresp2, pcresp3, pcresp4, pcresp5  ]
+fig3, grafdemora = plt.subplots()
+grafdemora.set_title("Tempo de demora para ser atendido presencialmente")
+grafdemora.pie(tamanho, autopct='%1.1f%%',shadow=False, startangle=90)
+grafdemora.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+grafdemora.legend(nomes)
+#=====================================Grafico de Pizza do tempo demora solucoes=======================================================
+
+#=====================================Grafico de Pizza do tempo demora solucoes=======================================================
+nomes = '< 10 minutos', '10 - 30 minutos', '30 - 45 minutos', '1 hora', '> 1 hora'
+tamanho = [pcrespcel1, pcrespcel2, pcrespcel3, pcrespcel4, pcrespcel5  ]
+fig4, grafdemoracel = plt.subplots()
+grafdemoracel.set_title("Tempo de demora para ser atendido no telefone")
+grafdemoracel.pie(tamanho, autopct='%1.1f%%',shadow=False, startangle=90)
+grafdemoracel.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+grafdemoracel.legend(nomes)
+
+#===================================== Grafico Pizza do meio de transporte======================================
+nomes = 'Onibus', 'Carro', 'Van', 'Bicicleta', 'Outros'
+tamanho = [onibus,carro,van ,bicicleta ,outros]
+fig5, transporte = plt.subplots()
+transporte.set_title("Meios de Transporte dos clientes")
+transporte.pie(tamanho, autopct=make_autopct(tamanho),shadow=False, startangle=90)
+transporte.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+transporte.legend(nomes)
+#===================================== Grafico Pizza do meio de transporte======================================
+
+#===================================== Grafico Pizza das mudanças solicitadas======================================
+nomes = 'Equipe', 'Equipamentos', 'Procedimentos', 'Coordenação'
+tamanho = [equipe,equipamentos,procedimentos ,coordenacao]
+fig6, sugestao = plt.subplots()
+sugestao.set_title("Mudanças Solicitadas pelos funcionarios")
+sugestao.pie(tamanho,autopct=make_autopct(tamanho),shadow=False, startangle=90)
+sugestao.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+sugestao.legend(nomes)
+#===================================== Grafico Pizza das mudanças solicitadas======================================
+
+#===================================== Grafico Pizza dos dias da semana======================================
+nomes = 'Segunda', 'Terça', 'Quarta', 'Quinta','Sexta'
+tamanho = [segunda,terca,quarta ,quinta,sexta]
+fig7, diagraf = plt.subplots()
+diagraf.set_title("Relação de dia da semana que o pessoal mais frequenta o CAA\n*Segundo os funcionarios")
+diagraf.pie(tamanho,autopct=make_autopct(tamanho),shadow=False, startangle=90)
+diagraf.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+diagraf.legend(nomes)
+#===================================== Grafico Pizza dos dias da semana======================================
+
+#===================================== Grafico Pizza das semanas do mês======================================
+nomes = '1° semana', '2° semana', '3° semana', '4° semana',
+tamanho = [sem1,sem2,sem3 ,sem4]
+fig8, semanames = plt.subplots()
+semanames.set_title("Relação das semanas onde o CAA tem maior movimento\n*Segundo os funcionarios")
+semanames.pie(tamanho,autopct=make_autopct(tamanho),shadow=False, startangle=90)
+semanames.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+semanames.legend(nomes)
+#===================================== Grafico Pizza das semanas do mês======================================
+
+#===================================== Grafico Pizza das horarios de frequencia======================================
+nomes = '15:00h', '16:00h', '17:00h', '18:00h','19:00h','20:00h'
+tamanho = [hora1,hora2,hora3 ,hora4,hora5,hora6]
+fig9, semanames = plt.subplots()
+semanames.set_title("Horario que os clientes costumam frequentar o CAA")
+semanames.pie(tamanho,autopct=make_autopct(tamanho),shadow=False, startangle=90)
+semanames.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+semanames.legend(nomes)
+#===================================== Grafico Pizza das horarios de frequencia======================================
+
+#===================================== Grafico Pizza de satisfação dos funcionarios======================================
+nomes = 'Sim', 'Não'
+tamanho = [contadorlikefunc, contadordislikefunc]
+fig10, gostatrabalho = plt.subplots()
+gostatrabalho.set_title("Relação dos funcionarios que gostam de trabalhar no CAA")
+gostatrabalho.pie(tamanho,autopct=make_autopct(tamanho),shadow=False, startangle=90)
+gostatrabalho.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+gostatrabalho.legend(nomes)
+#===================================== Grafico Pizza de satisfação dos funcionarios======================================
+
+#===================================== Grafico Pizza de satisfação dos funcionarios======================================
+nomes = 'Sim', 'Não'
+tamanho = [contadorsatifacao, contadorinsatisfacao]
+fig11, gostacaa = plt.subplots()
+gostacaa.set_title("Relação da satisfação dos funcionarios com a situação do CAA")
+gostacaa.pie(tamanho,autopct=make_autopct(tamanho),shadow=False, startangle=90)
+gostacaa.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+gostacaa.legend(nomes)
+#===================================== Grafico Pizza de satisfação dos funcionarios======================================
+
+#===================================== Grafico Pizza dos problemas não resolvidos presencialmente======================================
+nomes = 'Boletos', 'Carimbo/Passe escolar','Requerimentos','Problema com matricula','Documentos','Cancelamento/Trancamento de matricula'
+tamanho = [grafnotsolbol, grafnotsolcarimbo,grafnotsolrequ,grafnotsolprob,grafnotsoldoc,grafnotsoltranc]
+fig12, notsolve = plt.subplots()
+notsolve.set_title("Relação dos serviços que não foram solucionados presencialmente")
+notsolve.pie(tamanho,labels =nomes,autopct=make_autopct(tamanho),shadow=False, startangle=90)
+notsolve.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+
+
+#===================================== Grafico Pizza dos problemas não resolvidos no telefone======================================
+nomes = 'Boletos', 'Carimbo/Passe escolar','Requerimentos','Problema com matricula','Documentos','Cancelamento/Trancamento de matricula'
+tamanho = [grafnotsolcelbol, grafnotsolcelcarimbo,grafnotsolcelrequ,grafnotsolcelprob,grafnotsolceldoc,grafnotsolceltranc]
+fig13, notsolvecel = plt.subplots()
+notsolvecel.set_title("Relação dos serviços que não foram solucionados presencialmente")
+notsolvecel.pie(tamanho,labels = nomes,autopct=make_autopct(tamanho),shadow=False, startangle=90)
+notsolvecel.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+
+#===================================== Grafico Pizza dos problemas não resolvidos no telefone======================================
+nomes = 'Boletos', 'Carimbo/Passe escolar','Requerimentos','Problema com matricula','Documentos','Cancelamento/Trancamento de matricula'
+tamanho = [grafservbol, grafservcar,grafservreq,grafservdoc,grafservprob,grafservtranc]
+fig14, grafserv = plt.subplots()
+grafserv.set_title("Relação dos serviços solicitados pelos clientes")
+grafserv.pie(tamanho,labels = nomes,autopct=make_autopct(tamanho),shadow=False, startangle=90)
+grafserv.axis('equal')  # Equal deixa o grafico sem ficar distorcido.
+
+
+#==========================================Salvando os graficos em um PDF==============================================
+pp = PdfPages('relatorio.pdf') #cria um PDF com o nome "relatório.pdf
+pp.savefig(fig14)#Relação dos serviços solicitados ao CAA (total)
+pp.savefig(fig1) #Contagem de solução 
+pp.savefig(fig12) #Relação problemas não resolvidos presencialmente
+pp.savefig(fig3) #Tempo de demora para ser atendido presencialmente
+
+pp.savefig(fig2) #Contagem de solução Telefonica
+pp.savefig(fig13) #Relação problemas não resolvidos no telefone
+pp.savefig(fig4) #Tempo de demora para ser atendido no telefone
+
+pp.savefig(fig9) #Horario que os clientes costumam frequentar o CA
+pp.savefig(fig7) #Relação de dia da semana que o pessoal mais frequenta o CAA
+pp.savefig(fig8) #Relação das semanas onde o CAA tem maior movimento
+pp.savefig(fig5) #Meios de Transporte dos clientes
+
+pp.savefig(fig10)#Relação dos funcionarios que gostam de trabalhar no CAA
+pp.savefig(fig11)#Relação da satisfação dos funcionarios com a situação do CAA
+pp.savefig(fig6) #Mudanças Solicitadas pelos funcionarios
+
+'''
+Codigo criado com intuição de fazer o datamining
+PDF acabou sendo criado por pagina separada,infelizmente o Join das paginas não teve tempo o suficiente para ser estudado
+O codigo esta disponivel no Github através do seguinte nome e link
+-------------------------------------------------------------------------------------------------------
+Thigofu/BusinessIntel_Modulo 
+https://github.com/Thigofu/BusinessIntel_Modulo
+'''
+pp.close() #encerra o processo salvando o PDF com o conteudo inserido
+#==========================================Salvando os graficos em um PDF==============================================
